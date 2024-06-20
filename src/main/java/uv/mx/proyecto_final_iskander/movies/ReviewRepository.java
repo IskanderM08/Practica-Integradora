35035c287d7ec7a532ba31866f775350480aa43f
IskanderM08 <IskanderMercader2002@gmail.com>
2024-06-20 13:06:25 -0600

    Estructura del Proyecto

---
+++ src/main/java/uv/mx/proyecto_final_iskander/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package uv.mx.proyecto_final_iskander.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
